43be32ad03ac5af08020d1f8082461472e115957
Gladiaturuss <artem_potapov_1975@mail.ru>
2021-12-16 00:35:38 +0300

    Доработана 3-2

--- Task3-2/main.cpp
+++ Task3-2/main.cpp
@@ -1,26 +1,28 @@
-#include <cmath>
+п»ї#include <cmath>
 #include <iostream>
-#define eps 0.0001
 using namespace std;
+
+const double eps = 0.0001;
+
 int main()
 {
     double sum;
     sum = 0;
     const int m = 10;
-    int n, i, k;
-    float tsum = 0, sum2 = 0;
+    int n;
+    double tsum = 0, sum2 = 0;
     cout << "n: ";
     cin >> n;
 
-    //вычисление суммы с 0-го слагаемого
+    //РІС‹С‡РёСЃР»РµРЅРёРµ СЃСѓРјРјС‹ СЃ 0-РіРѕ СЃР»Р°РіР°РµРјРѕРіРѕ
     for (i = 1, k = 1; i <= n; i++, k += 2)
         tsum += (float)i / (float)k;                                           
 
-//вычисление суммы с n-го слагаемого
+//РІС‹С‡РёСЃР»РµРЅРёРµ СЃСѓРјРјС‹ СЃ n-РіРѕ СЃР»Р°РіР°РµРјРѕРіРѕ
     for (i = n, k = 2 * n - 1; i > 0; i--, k -= 2)
         sum2 += (float)i / (float)k;
 
-    //печать результатов
+    //РїРµС‡Р°С‚СЊ СЂРµР·СѓР»СЊС‚Р°С‚РѕРІ
     cout << "sum: " << tsum << endl;
     cout << "sum2: " << sum2 << endl;
 
